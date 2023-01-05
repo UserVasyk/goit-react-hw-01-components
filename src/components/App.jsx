a940c809d6c07635a6275b12a21b59a838490061
UserVasyk <106909139+UserVasyk@users.noreply.github.com>
2023-01-05 12:13:58 +0200

    add Profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import PropTypes from 'prop-types';
+import { Profile } from './Profile/Profile';
+import user from '../path/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
+};
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+export const Profile = ({ avatar, location, stats, tag, username }) => {
+  return (
+    <div>
+      <div>
+        <img width="200" src={avatar} alt="User avatar" />
+        <p>{username}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
